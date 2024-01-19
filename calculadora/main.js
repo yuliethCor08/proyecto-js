76a114e53e10077c4ac8ac3550451d9410dad6a8
yuliethCor08 <yuliethcorrea08@gmail.com>
2024-01-19 12:44:46 -0500

    proyecto js

---
+++ calculadora/main.js
@@ -0,0 +1,28 @@
+const pantalla = document.querySelector(".pantalla");
+const botones = document.querySelectorAll(".btn");
+
+botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+        const botonApretado = boton.textContent;
+
+        if (boton.id === "c") {
+            pantalla.textContent = "0";
+            return;
+        }
+
+        if (boton.id === "igual") {
+            try {
+                pantalla.textContent = eval(pantalla.textContent);
+            } catch {
+                pantalla.textContent = "Error!";
+            }
+            return;
+        }
+
+        if (pantalla.textContent === "0" || pantalla.textContent === "Error!") {
+            pantalla.textContent = botonApretado;
+        } else {
+            pantalla.textContent += botonApretado;
+        }
+    })
+})
